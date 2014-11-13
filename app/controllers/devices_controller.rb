9ce7f524c1659eaafad3d048a40e7352da2739c0
CJ Avilla <cjavilla@gmail.com>
2014-11-12 17:07:30 -0800

    remove routes for devices

--- app/controllers/devices_controller.rb
+++
@@ -1,7 +0,0 @@
-class DevicesController < ApplicationController
-  before_action :require_user!
-
-  def index
-    @devices = current_user.devices
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'devices/index'
-
   root to: 'static_pages#root', via: :get
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-  resources :devices, only: [:index]
 end
